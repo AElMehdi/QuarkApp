d6cdde29c304e399c32af7ded125a0b43a7c661d
AElMehdi <assirelmehdi@gmail.com>
2021-11-29 21:35:41 +0100

    Add a service + jenv for java 17

--- src/test/java/com/aelmehdi/GreetingResourceTest.java
+++ src/test/java/com/aelmehdi/GreetingResourceTest.java
@@ -3,7 +3,6 @@ package com.aelmehdi;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
-import static com.sun.org.apache.xerces.internal.util.PropertyState.is;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
@@ -13,10 +12,10 @@ public class GreetingResourceTest {
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("Hello RESTEasy"));
+                .when().get("/hello")
+                .then()
+                .statusCode(200)
+                .body(is("Hello RESTEasy"));
     }
 
 }
